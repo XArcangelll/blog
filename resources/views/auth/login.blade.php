5f66621491f893c5279f2a04bf2e6e752467b4ec
XArcangelll <Xx-Arcangel@hotmail.com>
2023-11-21 21:03:40 -0500

    no funciono

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
             </div>
         @endif
 
-        <form method="POST" action="{{ secure_url('login') }}">
+        <form method="POST" action="{{ route('login') }}">
             @csrf
 
             <div>
