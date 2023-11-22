ae35ee93d78ea2a17e567fb2466b171571f89bc4
XArcangelll <Xx-Arcangel@hotmail.com>
2023-11-21 19:11:29 -0500

    arreglado

--- routes/admin.php
+++ routes/admin.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\RoleController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('', [HomeController::class, 'index'])->middleware('can:admin.home')->name('admin.home');
+Route::get('', [HomeController::class, 'index'])->name('admin.home');
 
 
 Route::resource('users', UserController::class)->only(['index', 'edit', 'update'])->names('admin.users')->missing(function () {
